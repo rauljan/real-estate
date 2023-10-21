90467b6cd0557ff73905bdfb584e3068359bd899
Raul Ismailov <74872847+rauljan@users.noreply.github.com>
2023-10-21 05:07:42 +0300

    Add files via upload

---
+++ src/properties.js
@@ -0,0 +1,35 @@
+import house1 from "./media/image1.png";
+import house2 from "./media/image2.png";
+import house3 from "./media/image3.png";
+
+export const properties = [
+  {
+    id: "1",
+    img: house1,
+    price: "35000",
+    address: "Los-Santos",
+    bedrooms: 2,
+    bathrooms: 2,
+    space: 2000,
+  },
+
+  {
+    id: "2",
+    img: house2,
+    price: "38000",
+    address: "San-Antonio",
+    bedrooms: 3,
+    bathrooms: 2,
+    space: 2300,
+  },
+
+  {
+    id: "3",
+    img: house3,
+    price: "47000",
+    address: "Las-Venturas",
+    bedrooms: 4,
+    bathrooms: 3,
+    space: 3000,
+  },
+];
